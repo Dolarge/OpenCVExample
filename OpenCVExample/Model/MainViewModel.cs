5dd53c1fd42161c19be569e4556db2da2069af3c
dh.cheon <dohyun722@daum.net>
2022-01-11 17:13:10 +0900

    feature : setup cam

---
+++ OpenCVExample/Model/MainViewModel.cs
@@ -0,0 +1,50 @@
+﻿using OpenCvSharp;
+using System.Linq;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace OpenCVExample.Model
+{
+    public class MainViewModel
+    {
+        VideoCapture cap;
+        WriteableBitmap wb;
+        const int frameWidth = 640;
+        const int frameHeight = 480;
+        bool loop = false;
+
+
+        public MainViewModel()
+        {
+            
+        }
+
+        public void WindowLoad()
+        {
+
+        }
+
+        public bool initWebCamera()
+        {
+            try
+            {
+                cap = VideoCapture.FromCamera(CaptureDevice.Any, 0);
+                cap.FrameWidth = frameWidth;
+                cap.FrameHeight = frameHeight;
+                cap.Open(0);
+                wb = new WriteableBitmap(cap.FrameWidth, cap.FrameHeight, 96, 96, PixelFormats.Bgr24, null);
+                Image.Source = wb;
+
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+
+
+        }
+
+    }
+}
