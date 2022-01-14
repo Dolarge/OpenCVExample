34f5653d2527cec423ce9b63726b434403418c7c
dh.cheon <dohyun722@daum.net>
2022-01-14 09:40:52 +0900

    feature : load video fin

--- OpenCVExample/Model/runVideoViewModel.cs
+++ OpenCVExample/Model/runVideoViewModel.cs
@@ -5,13 +5,30 @@ namespace OpenCVExample.Model
     public class runVideoViewModel
     {
 
-        VideoCapture Capture = new VideoCapture(/* 비디오 주소*/);
-        Mat frame = new Mat();
+        public void viedoRun()
+        {
+            //video 초기화
+            //""안에 경로
+            VideoCapture VideoCapture = new VideoCapture("");
+            
+            //프레임 표시를 위해 선언
+            Mat frame = new Mat();
 
-      while (true)
-	{
+            //현재 프레임이 동영상 파일의 총프레임과 같아질때까지
+            while (VideoCapture.PosFrames != VideoCapture.FrameCount)
+            {
+                //Frame load
+                VideoCapture.Read(frame);
+                //표시
+                Cv2.ImShow("frame", frame);
+                //특정 시간만큼 대기
+                Cv2.WaitKey(33);
+            }
 
-	}
+            frame.Dispose();
+            VideoCapture.Release();
+            Cv2.DestroyAllWindows();
 
+        }
     }
 }
